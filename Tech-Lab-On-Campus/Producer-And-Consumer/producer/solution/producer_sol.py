76317f446f1caca7f7105bfcbca6b8f1cc6024ac
Qing Shi <qingshi@seas.upenn.edu>
2024-02-23 16:47:51 -0500

    Add producer code

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -0,0 +1,28 @@
+import pika
+import os
+
+class mqProducer:
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        connection = pika.BlockingConnection(parameters=conParams)
+        
+        # Establish Channel
+        self.channel = connection.channel()
+        self.channel.exchange_declare('Test Exchange')
+
+        # Create the exchange if not already present
+        exchange = self.channel.exchange_declare(exchange=self.exchange_name)
+
+    def publishOrder(self, message: str) -> None:
+        # Basic Publish to Exchange
+        self.channel.basic_publish(
+            exchange=self.exchange_name,
+            routing_key=self.routing_key,
+            body=message
+        )
